119959d6fcc34ff0dcf2dc22f55505be6d5cbafe
RudranshJaiswal01 <rudranshonpc02@gmail.com>
2025-06-15 07:54:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
 def preprocess_data(df):
     indices = df.index
     df = df.drop(columns=['customerID', 'TotalCharges'], errors='ignore')
